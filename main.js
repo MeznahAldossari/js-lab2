d6d9db13811311f94420ef995bf36c2d05a1636d
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-05-22 17:04:53 +0300

    v2

---
+++ main.js
@@ -0,0 +1,15 @@
+for (i = 0; i <= 25; i++) {
+  if (i % 2 == 0) {
+    console.log(`even numbers:${i}`);
+  }
+}
+let res;
+for (i = 1; i <= 10; i++) {
+  console.log(`Square:${i ** 2}`);
+}
+
+for (i = 0; i <= 20; i++) {
+  if (i % 2 != 0) {
+    console.log(`odd numbers:${i}`);
+  }
+}
